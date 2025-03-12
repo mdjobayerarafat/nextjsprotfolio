7fda9b52d48a6532ac910d2ae3d4cf85e2ecd209
mdjobayerarafat <mdjobayerarafat@gmail.com>
2025-03-12 19:20:22 -0400

    update blogwrf

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '20.163.180.176'
-      },
-      {
-        protocol: 'https',
-        hostname: '20.163.180.176'
-      },
-      {
-        protocol: 'https',
-        hostname: 'mdjobayerarafat.live'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.mdjobayerarafat.live'
-      }
-    ],
-    unoptimized: false
-  },
-  typescript: {
-    ignoreBuildErrors: true
-  }
-};
+        const nextConfig = {
+          images: {
+            domains: ['20.163.180.176', 'mdjobayerarafat.live', 'www.mdjobayerarafat.live'],
+            remotePatterns: [
+              {
+                protocol: 'http',
+                hostname: '20.163.180.176',
+                pathname: '/static/**'
+              },
+              {
+                protocol: 'http',
+                hostname: '20.163.180.176',
+                pathname: '/static/uploads/**'
+              },
+              {
+                protocol: 'https',
+                hostname: '20.163.180.176',
+                pathname: '/static/**'
+              },
+              {
+                protocol: 'https',
+                hostname: 'mdjobayerarafat.live',
+                pathname: '/static/**'
+              },
+              {
+                protocol: 'https',
+                hostname: 'www.mdjobayerarafat.live',
+                pathname: '/static/**'
+              }
+            ]
+          },
+          typescript: {
+            ignoreBuildErrors: true
+          }
+        };
 
-module.exports = nextConfig;
+        module.exports = nextConfig;
