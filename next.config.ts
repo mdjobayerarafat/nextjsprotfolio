004c0f7f825b3b4e52db88d9cf92bc62dc709e72
mdjobayerarafat <mdjobayerarafat@gmail.com>
2025-03-13 11:16:37 -0400

    final update2

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 const nextConfig = {
   images: {
     unoptimized: true, // Disable Next.js image optimization
-    domains: ['20.163.180.176', 'mdjobayerarafat.live', 'www.mdjobayerarafat.live'],
+    domains: ['20.163.180.176', 'mdjobayerarafat.live', 'www.mdjobayerarafat.live', '20.197.36.135'],
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: '20.163.180.176',
+        hostname: 'http://20.197.36.135',
         pathname: '/static/**',
       },
       {
