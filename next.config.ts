8d9087c46162b79f626af8ce448ab91b9cdff908
mdjobayerarafat <mdjobayerarafat@gmail.com>
2025-03-13 05:12:18 -0400

    update blogwrf hostnazmer

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,22 @@
-/** @type {import('next').NextConfig} */
-                const nextConfig = {
-                  images: {
-                    domains: ['20.163.180.176', 'mdjobayerarafat.live', 'www.mdjobayerarafat.live'],
-                    remotePatterns: [
-                      {
-                        protocol: 'http',
-                        hostname: '20.163.180.176',
-                        pathname: '/static/**'
-                      },
-                      {
-                        protocol: 'http',
-                        hostname: '20.163.180.176',
-                        pathname: '/static/uploads/**'
-                      },
-                      {
-                        protocol: 'https',
-                        hostname: '20.163.180.176',
-                        pathname: '/static/**'
-                      },
-                      {
-                        protocol: 'https',
-                        hostname: 'mdjobayerarafat.live',
-                        pathname: '/static/**'
-                      },
-                      {
-                        protocol: 'https',
-                        hostname: 'www.mdjobayerarafat.live',
-                        pathname: '/static/**'
-                      }
-                    ]
-                  },
-                  typescript: {
-                    ignoreBuildErrors: true
-                  }
-                };
+const nextConfig = {
+  images: {
+    domains: ['20.163.180.176', 'mdjobayerarafat.live', 'www.mdjobayerarafat.live'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '20.163.180.176',
+        pathname: '/static/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mdjobayerarafat.live',
+        pathname: '/static/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-                module.exports = nextConfig;
+module.exports = nextConfig;
